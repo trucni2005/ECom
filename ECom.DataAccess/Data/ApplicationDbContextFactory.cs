5a408037e7cef7acabbc214a33a46e1f437a34ce
mkhoatd <binnhatle@gmail.com>
2022-06-29 07:36:07 +0700

    add docker file and fix project name

--- ECom.DataAccess/Data/ApplicationDbContextFactory.cs
+++ ECom.DataAccess/Data/ApplicationDbContextFactory.cs
@@ -9,7 +9,7 @@ public class ApplicationDbContextFactory:IDesignTimeDbContextFactory<Application
     public ApplicationDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        optionsBuilder.UseSqlServer("Server=.;Database=EComWeb;Trusted_Connection=True");
+        optionsBuilder.UseSqlServer("Server=.;Database=ECom.Web;Trusted_Connection=True");
         return new ApplicationDbContext(optionsBuilder.Options);
 
     }
